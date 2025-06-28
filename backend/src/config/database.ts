0cf565652a9cd0dfa8a86bf618153cc6dc95a639
caua <cauamilmilhoes@gmail.com>
2025-06-27 23:41:36 -0300

    Criei uma conexão com o db

---
+++ backend/src/config/database.ts
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+
+export default async function connect():Promise<void> {
+    const client = new Client({
+        user: process.env.DB_User,
+        password: process.env.DB_Password,
+        host: process.env.DB_Host,
+        port: process.env.PG_Port,
+        database: process.env.DB_Name
+    });
+
+    await client.connect();
+    
+    try {
+        const result = await client.query('SELECT $1::text as message', ['Hello world!'])
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.end();
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import dbConfig from './config/database';
+
+dbConfig()
 
 const app = express();
 
