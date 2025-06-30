1ab4f506459465c9b7078128e4a536cc56021a16
caua <cauamilmilhoes@gmail.com>
2025-06-29 23:00:24 -0300

    Implementei o bcrypt.

--- backend/src/modules/auth.ts
+++ backend/src/modules/auth.ts
@@ -1,29 +1,39 @@
-import express from 'express';
+const express = require('express');
+const bcrypt = require('bcrypt');
 
 import database from '../config/database';
 
 const router = express.Router();
 
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+async function hash(senha: string): Promise<string> {
+    const saltRounds = 12;
+    const hash = await bcrypt.hash(senha, saltRounds);
+    return hash;
+}
+
 async function buscarUsuarioEmail(email: string): Promise<object> {
-    const tst = await database('SELECT * FROM usuarios WHERE email = $1',[email]);
+    const tst = await database('SELECT * FROM usuarios WHERE email = $1;',[email]);
     return await tst.rows[0];
 };
 
-router.post("/backend/criarLogin",(req,res):void => {
+router.post("/backend/criarLogin",(req: any,res: any):void => {
     const email: string = req.body.email;
     const senha: string = req.body.senha;
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if(regex.test(email)) {
         buscarUsuarioEmail(email).then((user: object | undefined) => {
             if(typeof(user) == "undefined") {
-                database(
-                    'INSERT INTO usuarios(email, senha, role) VALUES($1, $2, $3);',
-                    [email, senha, 0]
-                );
-                res.sendStatus(201);
+                hash(senha).then((hash) => {
+                    database(
+                        'INSERT INTO usuarios(email, senha, role) VALUES($1, $2, $3);',
+                        [email, hash, 0]
+                    );
+                    res.sendStatus(201);
+                })
             }else {
-                res.sendStatus(409);
+                console.log(typeof(res.sendStatus(409)));
             }
         }).catch((err: object) => {
             console.log(err);
