bbb440190cfa0dcaaa040823c6d4bf7026bdae32
caua <cauamilmilhoes@gmail.com>
2025-06-30 21:09:22 -0300

    Tratei o erro de requisição malformada.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,12 @@ const app = express();
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json());
 
+// Tratamento de erros global
+app.use((err: any, _: any, res: any, next: any) => {
+    res.status(400)
+    .json({"status": "Requisição inválida", "erro:": err.message});
+});
+
 app.use("/",user);
 app.use("/",auth);
 
